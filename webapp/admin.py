34239fec6e0d0a4d386fbfcf008b7d2ec4a6413a
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-01-13 13:33:06 +0600

    Созданы модели Task, Status, Type. Зарегистрированы в admin.py, в админ панели джанго добавлены записи статус, тип и добавлены задачи. Применена миграция.

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from webapp.models import Task, Status, Type
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'status', 'type']
+    list_filter = ['status', 'type']
+    search_fields = ['title', 'status', 'type']
+    fields = ['title', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['status']
+    fields = ['status']
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['type']
+    fields = ['type']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Краткое описание")
+    description = models.CharField(max_length=2000, null=True, blank=True, verbose_name="Полное описание")
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, related_name='statuses', verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, related_name='types', verbose_name='Тип задачи')
+
+    def __str__(self):
+        return f"{self.pk}. {self.title} - {self.status}"
+
+    class Meta:
+        db_table = 'Tasks'
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+
+class Status(BaseModel):
+    status = models.CharField(max_length=200, null=False, blank=False, verbose_name="Статус")
+
+    def __str__(self):
+        return f"{self.status}"
+
+    class Meta:
+        db_table = 'Statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Type(BaseModel):
+    type = models.CharField(max_length=200, null=False, blank=False, verbose_name="Тип")
+
+    def __str__(self):
+        return f"{self.type}"
+
+    class Meta:
+        db_table = 'Types'
+        verbose_name = 'Тип задачи'
+        verbose_name_plural = 'Типы задач'
