73fd85b0ff5f5d0b147154cec34a0f2440a662c1
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-02-14 16:19:57 +0600

    Добавлен валидатор к полю first_name

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,4 +9,19 @@ class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
 
+    def clean_first_name(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data['first_name']
+        if first_name:
+            return cleaned_data
+        else:
+            raise ValidationError('Заполните это поле!')
+
+    def clean_first_name(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data['first_name']
+        if first_name:
+            return cleaned_data
+        else:
+            raise ValidationError('Пожалуйста заполните это поле!')
 
