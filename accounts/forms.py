d063710eac974ecc7864b3701fca23bd02fdfb92
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-02-14 15:37:01 +0600

    Email field now is required

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
 
 
 class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Электронная почта")
+
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
+
+
