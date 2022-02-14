6bbe80dc9bcc7d7d6f59755f2b23da04f4501ed9
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-02-14 14:10:24 +0600

    Обновил фикстуру

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.forms import MyUserCreationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = "registration.html"
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:index')
+        return next_url
