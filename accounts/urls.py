319121fdbd456ed01d9f3be12f9a5dc8124826b3
tugolbay-subanbekov <subanbekovt@gmail.com>
2022-02-21 14:41:26 +0600

    Добавлена страница профиля пользователя

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserProfileView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('registration/', RegisterView.as_view(), name="registration"),
+    path('<int:pk>/', UserProfileView.as_view(), name="user_profile")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
+from accounts.models import Profile
 
 
 class RegisterView(CreateView):
@@ -14,6 +15,7 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        Profile.objects.create(user=user)
         login(self.request, user)
         return redirect(self.get_success_url())
 
@@ -24,3 +26,12 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('webapp:index')
         return next_url
+
+
+class UserProfileView(DetailView):
+    model = get_user_model()
+    template_name = 'profile.html'
+    context_object_name = 'user_object'
+    
+    def get_context_data(self, **kwargs):
+        return super(UserProfileView, self).get_context_data(**kwargs)
